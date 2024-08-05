1e6a406aa375a67e1f221167c4dd283f9b900f35
guicavalc <guicavalc@hotmail.com>
2024-08-05 08:55:53 -0300

    validação parte 3

--- BoloDaTia/script.js
+++ BoloDaTia/script.js
@@ -1,20 +1,35 @@
-//----------------------Validação USERNAME---------------//
+//----------------------Validação USUÁRIO, EMAIL (O Que?)---------------//
 let form = document.getElementById('form');
 let usernameInput = document.getElementById('username');
 let usernameLabel = document.querySelector('label[for="username"]');
 let usernameHelper = document.getElementById('username-helper');
+let emailInput = document.getElementById('email');
+let emailLabel = document.querySelector('label[for="email"]');
+let emailHelper = document.getElementById('email-helper');
+let senhaInput = document.getElementById('senha');
+let senhaLabel = document.querySelector('label[for="senha"]');
+let senhaHelper = document.getElementById('senha-helper');
+let confirmasenhaInput = document.getElementById('confirma-senha');
+let confirmasenhaLabel = document.querySelector('label[for="confirma-senha"]');
+let confirmasenhaHelper = document.getElementById('confirma-senha-helper');
 
 
-//---------------------Mostrar Popup campo obrigatório---//
+//---------------------Mostrar/ocultar Popup de campo obrigatório---//
+function mostrarPopup(input, label){
 
-usernameInput.addEventListener("focus", () => {
-    usernameLabel.classList.add("required-popup")
-})
+    input.addEventListener("focus", () => {
+        label.classList.add("required-popup")
+    });
+    
+    input.addEventListener("blur", () => {
+    label.classList.remove("required-popup")
+    });
+}
 
-//---------------------Oculta Popup campo obrigatório---//
-usernameInput.addEventListener("blur", () => {
-    usernameLabel.classList.remove("required-popup")
-})
+mostrarPopup(usernameInput, usernameLabel);
+mostrarPopup(emailInput, emailLabel);
+mostrarPopup(senhaInput, senhaLabel);
+mostrarPopup(confirmasenhaInput, confirmasenhaLabel);
 
 //---------------------Validar valor do input----------//
 usernameInput.addEventListener("change", function(e){
@@ -30,6 +45,22 @@ usernameInput.addEventListener("change", function(e){
         usernameInput.classList.remove("error")
         usernameInput.classList.add("correct")
         usernameHelper.classList.remove("visible")
+    }
+})
+
+emailInput.addEventListener("change", function(e){
+    let valor = e.target.value
+    if(valor.includes("@") && valor.includes(".com")){
+        console.log("Email válido")
+        emailInput.classList.remove("error")
+        emailInput.classList.add("correct")
+        emailHelper.classList.remove("visible")
+    }else{
+        console.log("Email incluir um @ e .com")
+        emailInput.classList.remove("correct")
+        emailInput.classList.add("error")
+        emailHelper.innerText = "Email precisa incluir um @ e .com"
+        emailHelper.classList.add("visible")
     }
 })
 
